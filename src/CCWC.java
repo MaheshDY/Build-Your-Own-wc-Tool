142d614037e24c1c8ab1b92b16d54040958e376d
Mahesh Devalokam Yoganand <mahesh.devalokam@c24.de>
2024-09-17 06:51:44 +0200

    Add compiled class and test.txt

--- src/CCWC.java
+++ src/CCWC.java
@@ -1,5 +1,64 @@
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class CCWC {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    try {
+      if (args.length == 1 && args[0].startsWith("-")) {
+        // Read from stdin (piped input)
+        processStdin(args[0]);
+      } else {
+        // Process file
+        processFile(args);
+      }
+    } catch (IOException e) {
+      System.err.println("Error reading input: " + e.getMessage());
+    }
+  }
+
+  private static void processStdin(String input) throws IOException {
+    BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(System.in));
+    Stream<String> lines = reader.lines();
+
+    if (input.equals("-l")) {
+      long lineCount = lines.count();
+      System.out.printf("%8d%n", lineCount);
+    }
+  }
+
+  private static void processFile(String[] args) throws IOException {
+    String fileName = args[args.length - 1];
+    Path filePath = Paths.get(fileName);
+
+    long byteCount = Files.size(filePath);
+    long lineCount = Files.lines(filePath).count();
+    long wordCount = Files.lines(filePath)
+        .flatMap(line -> Stream.of(line.split("\\s+")))
+        .filter(word -> !word.isEmpty())
+        .count();
+
+    if (args.length == 1) {
+      // No options provided, show all by default
+      System.out.printf("%8d %8d %8d %s%n", lineCount, wordCount, byteCount, fileName);
+    } else {
+      String option = args[0];
+      switch (option) {
+        case "-c" -> System.out.printf("%8d %s%n", byteCount, fileName);
+        case "-l" -> System.out.printf("%8d %s%n", lineCount, fileName);
+        case "-w" -> System.out.printf("%8d %s%n", wordCount, fileName);
+        case "-m" -> {
+          long charCount = Files.lines(filePath)
+              .mapToLong(String::length)
+              .sum();
+          System.out.printf("%8d %s%n", charCount, fileName);
+        }
+        default -> System.out.println("Unsupported option");
+      }
+    }
+
   }
 }
